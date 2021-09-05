a6ab3e46ec4a5564da9babab05868737edbe7c97
Ashik Chapagain <projectashik@gmail.com>
2021-09-05 18:41:56 +0545

    Allow only 2 origins

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+  "http://localhost:3000",
+  "https://hashnode-cards.herokuapp.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
